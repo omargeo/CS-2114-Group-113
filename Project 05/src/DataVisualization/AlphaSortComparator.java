214a1458fd900ccf81eefbd3b8c694843820d53e
Van-atVT <van7@vt.edu>
2021-04-19 12:48:58 -0400

    Change from interface to class
    Changed interface to class. added methods used for comparisons. check with TA on whether we can use comparison from strings class.

--- Project 05/src/DataVisualization/AlphaSortComparator.java
+++ Project 05/src/DataVisualization/AlphaSortComparator.java
@@ -1,9 +1,22 @@
 package DataVisualization;
+import java.util.Comparator;
 
+/**
+ * compares the names of races using the comparator class.
+ * @author Van Taylor (van7)
+ * @version 2021.04.19
+ */
+public class AlphaSortComparator implements Comparator<String> {
 
-public interface AlphaSortComparator {
-    
-    public int compare(String race1, Strings race2) {
-        //TODO
+    /**
+     * compares the two strings lexicographically
+     * @param race1 is the current string 
+     * @param race2 is the string to compare with the current string
+     * @return the relationship, -1, 0, or 1 based on the alphabetical 
+     * relationship race1 has with race2
+     */
+    @Override
+    public int compare(String race1, String race2) {
+        return(race1.compareToIgnoreCase(race2));
     }
 }
--- Project 05/src/DataVisualization/CFRSortComparator.java
+++ Project 05/src/DataVisualization/CFRSortComparator.java
@@ -1,8 +1,38 @@
 package DataVisualization;
+import java.util.Comparator;
+/**
+ * tried to pass double as param but you can't pass primatives into generics
+ * @author Van Taylor (van7)
+ * @version 2021.04.19
+ * @param <A>
+ */
+public class CFRSortComparator<A> implements Comparator<A> {
 
-public interface CFRSortComparator {
-
-    public int compare(String race1, Strings race2) {
-        //TODO
+    private final double THRESHOLD = 0.0001;
+    /**
+     * we must override the comparator class as double & not generic
+     * this is different from the generic values we wanted via the UML
+     * compare based on expectation the first double is higher than the second
+     * threshold is used for comparison
+     * @param race1
+     * @param race2
+     * @return the relationship between the two CFR values from the race class
+     * @precondition A is a double value
+     */
+    @Override
+    public int compare(A race1CFR, A race2CFR) {
+        double r1 = (double)race1CFR;
+        double r2 = (double)race2CFR;
+        if (r1 == r2) {
+            return 0;
+        }
+        boolean flag = true;
+        flag = (Math.abs(r1 - r2) < THRESHOLD);
+        if (flag) {
+            return 1;
+        }
+        else {
+            return -1;
+        }
     }
 }
